81129eafbcec3f96a2ecea441349005dc24875a3
fatihabdi <dotaawtf23@gmail.com>
2023-03-10 21:15:54 +0700

    Add M2

---
+++ Modul/M2.js
@@ -0,0 +1,30 @@
+var nama = "Rizky";
+var umur = 23.5;
+var TinggiBadan;
+TinggiBadan = 190;
+TinggiBadan = 200;
+var lulus = true;
+var pacar = ["Siti", "Rina", "Rani"];
+const beratbadan = 70;
+let hobi = "Membaca";
+function GenapGanjil(angka) {
+  if (angka % 2 == 0) {
+    return "Genap";
+  } else {
+    return "Ganjil";
+  }
+}
+document.getElementById("p1").innerHTML = "Hello World";
+document.getElementById("p1").style.color = "blue";
+// document.getElementsByClassName("p2").innerHTML = "Hello World";
+// document.getElementById("button").addEventListener("click", function () {
+//   console.log("Hello World");
+// });
+// document.querySelector("button").addEventListener("click", function () {
+//   console.log("Hello World");
+// });
+function Hallo() {
+  for (let i = 0; i < 5; i++) {
+    console.log("The number is " + i + "<br>");
+  }
+}
